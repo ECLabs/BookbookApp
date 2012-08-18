6b821246aa63e5ca3d7985e5cf57f6a2541f641a
ectech <jamil@evanschambers.com>
2012-08-17 21:00:39 -0400

    Formatting updates.  Changed user upload from a 2-step XHR process to a 1-step XHR process to eliminate timing issues.

--- Resources/child_windows/manage_profile.js
+++ Resources/child_windows/manage_profile.js
@@ -2,7 +2,26 @@ var win = Ti.UI.currentWindow;
 var currUser = Ti.App.CurrentUser;
 var REQUEST_TIMEOUT = Ti.App.REQUEST_TIMEOUT;
 
+
+var g_messageDialog = Ti.UI.createAlertDialog({
+	buttonNames: ['Continue'],
+	cancel:0,
+	title: 'Error',
+	message: 'There\'s an error.'
+});
+
+var spinnerButton = Titanium.UI.createButton({
+	systemButton:Titanium.UI.iPhone.SystemButton.SPINNER
+});
+
+function showMessageDialog(message) {
+	g_messageDialog.setMessage(message);
+	g_messageDialog.show();
+	
+}
+
 win.addEventListener('open', function(e) {
+	Ti.API.info('in OPEN handler on manage_profile.js window');
 	currUser = Ti.App.CurrentUser;
 	nameText.value = currUser.fullName;
 	bioArea.value = currUser.aboutMe;
@@ -10,6 +29,15 @@ win.addEventListener('open', function(e) {
 	locationText.value = currUser.location;
 });
 
+var g_doneDialog = Ti.UI.createAlertDialog({
+	buttonNames: ['Sign-in'],
+	cancel:0,
+	title: 'Success',
+	message: 'Your changes have been changed.'
+});
+g_doneDialog.addEventListener('click', function(e) {
+	win.close();
+});
 var scrollWin = Titanium.UI.createScrollView({
 	width: '100%',
 	height: '100%'
@@ -18,22 +46,12 @@ var scrollWin = Titanium.UI.createScrollView({
 
 var changepPhoto = false;
 	
-var saveProfileButton = Titanium.UI.createButton({
-title:'Save',
-height:25,
-width:'21%',
-top:0,
-right:'5%',
-borderWidth:4,
-color: '#000000'
-});
-	
 var profileManage = Titanium.UI.createLabel({
-text:'Manage Profile',
-top:'12%',
-height:'10%',
-left:'45%',
-font:{fontSize:18, fontStyle:'bold'}
+	text:'Manage Profile',
+	top:'12%',
+	height:'10%',
+	left:'45%',
+	font:{fontSize:18, fontStyle:'bold'}
 });
 	
 var userId = Titanium.UI.createLabel({  //This is a place holder so that our userId will be in scope
@@ -46,38 +64,46 @@ var userData;
 var resp;
 
 var profilePicManage = Titanium.UI.createImageView({
-image:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png',
-height:'30%',
-width:'35%',
-top:0,
-left:'5%',
+	image:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png',
+	height:'30%',
+	width:'35%',
+	top:0,
+	left:'5%'
 });
 
 
 profilePicManage.addEventListener('click', function(e)
 {
-	changepPhoto = true;
+	// display spinner in the top right navbar
+	win.setRightNavButton(spinnerButton);
 	
 	Titanium.Media.openPhotoGallery({
-			success:function(event) {
-				var image = event.media;
-			    Ti.API.info(image.height +' x '+ image.width);
-	        	profilePicManage.image = image;
-	        	Ti.API.info(image.height + " x " + image.width);        	   
-	  
-	        	profilePicManage.image = image;
-			}
-		});
+		success:function(event) {
+			var image = event.media;
+			
+		    Ti.API.info('Image dimensions - ' + image.height +' x '+ image.width);
+		    Ti.API.info('Image filesize - ' + image.length);
+		    
+		    win.setRightNavButton(save);
+		    
+		    if(image.length > 2097152) { 
+		    	showMessageDialog("The image you uploaded is greater than 2MB.  Unable to upload.");
+		    	return;
+		    }
+        	profilePicManage.image = image;
+        	changepPhoto = true;
+		}
+	});
 });
 	
 var aboutMeButton = Titanium.UI.createButton({
-title:'About Me',
-height:40,
-width:'45%',
-top:'32%',
-left:'5%',
-borderWidth:4,
-color: '#000000'
+	title:'About Me',
+	height:40,
+	width:'45%',
+	top:'32%',
+	left:'5%',
+	borderWidth:4,
+	color: '#000000'
 });
 	
 aboutMeButton.addEventListener('click', function(e)
@@ -92,13 +118,13 @@ aboutMeButton.addEventListener('click', function(e)
 });
 	
 var myReadingButton = Titanium.UI.createButton({
-title:'My Reading',
-height:40,
-width:'45%',
-top:'32%',
-right:'5%',
-borderWidth:4,
-color: '#000000'
+	title:'My Reading',
+	height:40,
+	width:'45%',
+	top:'32%',
+	right:'5%',
+	borderWidth:4,
+	color: '#000000'
 });
 	
 myReadingButton.addEventListener('click', function(e)
@@ -113,78 +139,78 @@ myReadingButton.addEventListener('click', function(e)
 });
 	
 var nameLabel = Titanium.UI.createLabel({
-text:'Name:',
-top:'44%',
-left:'10%',
-font:{fontSize:16}
+	text:'Name:',
+	top:'44%',
+	left:'10%',
+	font:{fontSize:16}
 });
 	
 var nameText = Titanium.UI.createTextField({
-editable: true,
-value: '',
-width:'80%',
-left:'10%',
-height: 25,
-top:'49%',
-font:{fontSize:14},
-color:'#000',
-textAlign:'left',
-returnKeyType:Titanium.UI.RETURNKEY_DONE,
-borderWidth:2,
-borderColor:'#000',
-borderRadius:0,
-suppressReturn:false,
-paddingLeft: 5
+	editable: true,
+	value: '',
+	width:'80%',
+	left:'10%',
+	height: 25,
+	top:'49%',
+	font:{fontSize:14},
+	color:'#000',
+	textAlign:'left',
+	returnKeyType:Titanium.UI.RETURNKEY_DONE,
+	borderWidth:2,
+	borderColor:'#000',
+	borderRadius:0,
+	suppressReturn:false,
+	paddingLeft: 5
 });
 	
 var locationLabel = Titanium.UI.createLabel({
-text:'Location:',
-top:'56%',
-left:'10%',
-font:{fontSize:16}
+	text:'Location:',
+	top:'56%',
+	left:'10%',
+	font:{fontSize:16}
 });
 	
 var locationText = Titanium.UI.createTextField({
-editable: true,
-value: '',
-width:'80%',
-left:'10%',
-top:'61%',
-height: 25,
-font:{fontSize:14},
-color:'#000',
-textAlign:'left',
-returnKeyType:Titanium.UI.RETURNKEY_DONE,
-borderWidth:2,
-borderColor:'#000',
-borderRadius:0,
-suppressReturn:false,
-paddingLeft: 5
+	editable: true,
+	value: '',
+	width:'80%',
+	left:'10%',
+	top:'61%',
+	height: 25,
+	font:{fontSize:14},
+	color:'#000',
+	textAlign:'left',
+	returnKeyType:Titanium.UI.RETURNKEY_DONE,
+	borderWidth:2,
+	borderColor:'#000',
+	borderRadius:0,
+	suppressReturn:false,
+	paddingLeft: 5
 });
 	
 var bioLabel = Titanium.UI.createLabel({
-text:'Biography:',
-top:'68%',
-left:'10%',
-font:{fontSize:16}
+	text:'Biography:',
+	top:'68%',
+	left:'10%',
+	font:{fontSize:16}
 });
 	
 var bioArea = Titanium.UI.createTextArea({
-editable: true,
-value: '',
-height:'25%',
-width:'80%',
-left:'10%',
-top:'73%',
-font:{fontSize:14},
-color:'#000',
-textAlign:'left',
-returnKeyType:Titanium.UI.RETURNKEY_DONE,
-borderWidth:2,
-borderColor:'#000',
-borderRadius:0,
-suppressReturn:false,
-paddingLeft: 5
+	editable: true,
+	value: '',
+	height:'25%',
+	width:'80%',
+	left:'10%',
+	top:'73%',
+	font:{fontSize:14},
+	color:'#000',
+	textAlign:'left',
+	returnKeyType:Titanium.UI.RETURNKEY_DONE,
+	borderWidth:2,
+	borderColor:'#000',
+	borderRadius:0,
+	suppressReturn:false,
+	paddingLeft: 5
 });
 
 bioArea.addEventListener('return', function()
@@ -211,17 +237,19 @@ var dataManage = [
 
 // create table view
 var tableviewManage = Titanium.UI.createTableView({
-data:dataManage,
-font:{fontSize:11},
-top:'49%',
-width: '90%',
-height: 105,
-borderWidth:1,
-borderColor: '#888'
+	data:dataManage,
+	font:{fontSize:11},
+	top:'49%',
+	width: '90%',
+	height: 105,
+	borderWidth:1,
+	borderColor: '#888'
 });
+
+
 	
 var winview = Ti.UI.createView({backgroundColor:'white'});
-winview.add(saveProfileButton);
+//winview.add(saveProfileButton);
 winview.add(profilePicManage);
 winview.add(profileManage);
 winview.add(aboutMeButton);
@@ -238,25 +266,36 @@ winview.add(userId);
 tableviewManage.hide(); //Initial state
 scrollWin.add(winview);
 Ti.UI.currentWindow.add(scrollWin);
-	
-var navActInd = Titanium.UI.createActivityIndicator();
-	
-saveProfileButton.addEventListener('click', function(e)
+		
+// add a save button to the right nav bar
+var save = Titanium.UI.createButton({
+	systemButton:Titanium.UI.iPhone.SystemButton.SAVE
+});
+win.setRightNavButton(save);	
+save.addEventListener('click', function(e)
 {
+	// display spinner in the top right navbar
+	win.setRightNavButton(spinnerButton);
 		
 	var url = Ti.App.SERVICE_BASE_URL + 'user/update/userId-'+userId.value;
 	var xhr = Titanium.Network.createHTTPClient();
-	xhr.setTimeout(REQUEST_TIMEOUT); // 10 second timeout
+	// Have to extend the timeout... pictures are taking a really long time to upload
+	xhr.setTimeout(40000); // 40 second timeout
+	//xhr.setTimeout(REQUEST_TIMEOUT); // 10 second timeout
 	xhr.onerror = function() {
-		showValidationErrorDialog("BookUp Web Services are currently unavailable.  Please try again soon.");
+		win.setRightNavButton(save);
+		showMessageDialog("BookUp Web Services are currently unavailable.  Please try again soon.");
+		
 	}
 	xhr.onload = function() {
+		win.setRightNavButton(save);
 	    var resp = this.responseText;  
 	    Ti.API.info(resp);
 	    
 	    var user = JSON.parse(resp);
 	    Ti.App.CurrentUser = user;
 	    Ti.App.fireEvent('saveProfileEvent');
+	    g_doneDialog.show();
 	}
 		
 	xhr.open('POST', url);
@@ -265,50 +304,19 @@ saveProfileButton.addEventListener('click', function(e)
 	currUser.fullName = nameText.value;
 	currUser.location = locationText.value;
 		
+	if(changepPhoto == true)
+	{	
+		//var img = profilePicManage.toBlob();
+		
+		currUser.picture = Ti.Utils.base64encode(profilePicManage.toBlob().imageAsResized(100,100)).toString();
+	}
+	
 	var obj = ({'jsondata':currUser});
 	Ti.API.info('JSON data about to be sent for update user -->');
-	Ti.API.info(JSON.stringify(obj));
+	//Ti.API.info(JSON.stringify(obj)); -- this logging statement takes a long time to display
 		
 	xhr.send(obj); 
-		
-		
-	if(changepPhoto == true)
-	{
-		var urlPhoto = Ti.App.SERVICE_BASE_URL + 'user/userId-'+currUser.userId+'/photo';
-	    Ti.API.info('Preparing to send data to: ' + urlPhoto);
-			
-		var xhr3 = Titanium.Network.createHTTPClient();
-	    xhr3.open("POST",urlPhoto);  
-	    xhr3.setTimeout(REQUEST_TIMEOUT); // 10 second timeout
-	    xhr3.send({myFile:profilePicManage.image});
-		xhr3.onerror = function()  {
-			win.setRightNavButton(done);
-			showValidationErrorDialog("Sorry, we could not upload this image.");
-		}
-				
-	    xhr3.onload = function() {
-	    	win.setRightNavButton(done);
-			var resp = this.responseText;  
-			Ti.API.info(resp);
-			if(!resp) { // no data returned means it was a success
-				g_doneDialog.show();
-				return;
-			}
-			else { // otherwise, there was an error
-				showValidationErrorDialog(resp);
-			}   	
-	    }
-     }
-		
-	Ti.UI.currentWindow.setRightNavButton(navActInd);
-	navActInd.show();
-	setTimeout(function()
-	{
-		navActInd.hide();
-		Ti.UI.currentWindow.setRightNavButton(null);
-		Ti.App.fireEvent('saveProfileEvent');
-		Ti.UI.currentWindow.close();
-	},3000);
+
 });
 	
 	
